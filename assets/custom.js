d9cd7ef6b4fe178511ab0e4b56da1cb0c5a5f022
gemichelst <empunktempunkt@gmail.com>
2021-05-12 15:05:58 +0200

    load custom.js

---
+++ assets/custom.js
@@ -0,0 +1,17 @@
+const debug = true;
+var curWinWidth = $(window).width(),
+    curWinHeight = $(window).height();
+
+
+
+$(document).ready(function() {
+
+
+console.log('filebrowser-geMichelst v1');
+
+
+
+
+
+
+});
